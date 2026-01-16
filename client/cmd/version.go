f49ab58a3665ab18bb69d4a78f17e3fb954030f4
Ovais Tariq <ot@tigrisdata.com>
2026-01-16 13:23:44 -0800

    feat: add version information for v1.0.0 release (#105)
    - Add version.go with Version, Commit, BuildTime variables to server
    - Add -version flag to ocache server binary
    - Add version subcommand to ocachecli CLI
    - Update Makefile to inject version info via ldflags at build time
    
    Version information is injected at build time using:
    - VERSION: from git tags (or "dev" if no tags)
    - COMMIT: short git commit hash
    - BUILD_TIME: UTC timestamp

---
+++ client/cmd/version.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Version information set at build time via ldflags
+var (
+	Version   = "dev"
+	Commit    = "unknown"
+	BuildTime = "unknown"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("ocachecli version %s\n", Version)
+		fmt.Printf("  commit: %s\n", Commit)
+		fmt.Printf("  built:  %s\n", BuildTime)
+	},
+}
